6de549bd2f635a914316826b4fadd92a48616e69
norbjert <norbert88@protonmail.com>
2026-04-23 21:05:37 +0200

    version bumps

--- src/renderer/store/configureStore.tsx
+++ src/renderer/store/configureStore.tsx
@@ -1,6 +1,6 @@
 // configureStore.js
 
-import { createStore } from 'redux'
+import { legacy_createStore as createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import rootReducers from './reducer'
