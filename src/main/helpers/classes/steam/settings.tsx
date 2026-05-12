225c492195056ab8477fd0cc29fbfb8a65beaf54
norbjert <norbert88@protonmail.com>
2026-05-12 22:01:52 +0200

    removing deprecated function calls, various code improvements

--- src/main/helpers/classes/steam/settings.tsx
+++ src/main/helpers/classes/steam/settings.tsx
@@ -3,17 +3,17 @@ import { safeStorage } from 'electron';
 import axios from 'axios';
 
 import { DOMParser } from '@xmldom/xmldom';
-async function getURL(steamID) {
+async function getURL(steamID: any) {
   return new Promise((resolve) => {
     axios
-      .get(`http://steamcommunity.com/profiles/${steamID}/?xml=1`)
+      .get(`https://steamcommunity.com/profiles/${steamID}/?xml=1`)
       .then(function (response) {
         const parser = new DOMParser();
         resolve(
           parser
             .parseFromString(response.data, 'text/xml')
             .getElementsByTagName('profile')[0]
-            .getElementsByTagName('avatarMedium')[0]?.childNodes[0]?.nodeValue
+            .getElementsByTagName('avatarMedium')[0]?.childNodes[0]?.nodeValue,
         );
       });
   }).catch((error) => console.log(error.message));
