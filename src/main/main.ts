e87842b774b6befd2ad989b9c8efa58b469ae28b
norbjert <norbert88@protonmail.com>
2026-05-21 22:44:45 +0200

    removing deprecated call

--- src/main/main.ts
+++ src/main/main.ts
@@ -613,7 +613,6 @@ async function cancelLogin(user: {
 }) {
   console.log('Cancel login');
   user.removeAllListeners('loggedOn');
-  user.removeAllListeners('loginKey');
   user.removeAllListeners('steamGuard');
   user.removeAllListeners('error');
 }
